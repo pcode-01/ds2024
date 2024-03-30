209c6746f61485d3b20b1962417221bd0cb48c4e
pcode-01 <pcode8451@gmail.com>
2024-03-30 22:20:38 +0900

    lru_sim.py added

--- lru_sim/lru_sim.py
+++ lru_sim/lru_sim.py
@@ -24,7 +24,7 @@ class CacheSimulator:
         print("cache_slot =", self.cache_slots, "cache_hit =", self.cache_hit, "hit ratio =", hit_ratio)
 
 if __name__ == "__main__":
-    data_file = open("./linkbench.trc")
+    data_file = open("./lru_sim/linkbench.trc")
     lines = data_file.readlines()
     for cache_slots in range(100, 1001, 100):
         cache_sim = CacheSimulator(cache_slots)
